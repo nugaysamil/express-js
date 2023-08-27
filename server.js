9d13fe55f21820812b84ec1aad52b2a66663c8fa
Şamil Nugay <samil_nugay@hotmail.com>
2023-08-27 20:26:43 +0300

    middleware impelements

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const errorHandler = require('./middleware/errorHandler');
+const {logger} = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
+// custom middleware
+app.use(logger);
+
+// cross origin resource sharing
+const whilelist = ['https://www.google.com','http://127.0.0.1:5500','http://localhost:3500'];
+const corsOptions = {
+  origin: (origin,callback) => {
+    if(whilelist.indexOf(origin) !== -1 || !origin) {
+      callback(null,true) 
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+
+
+// build-in middleware to handle urlencoded data
+// in other word, from data:
+// 'content-type: application/x-www.form-urlencoded'
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('^/$|/index.(.html)?',(req,res) => {
   res.sendFile('./views/index.html',{root:__dirname});
@@ -41,14 +72,20 @@ const three = (req,res) => {
 }
 
 app.get('/chain(.html)?', [one,two,three])
-app.get('/new-page(.html)?',(req,res) => {
-  res.status(400).sendFile(path.join(__dirname,'views','new-page.html'));
+
+app.all('*',(req,res) => {
+  res.status(404);
+  if(req.accepts('html')) {
+    res.sendFile(path.join(__dirname,'views','404.html'));
+    
+  }
+  else if(req.accepts('html')) {
+    res.json({error: '404 Not found'})
+  } else {
+    res.type('txt').send('404 Not found')
+  }
 }); 
 
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
-
-
-
-
-
-
