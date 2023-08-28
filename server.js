994e1ff1e237e1f1eb355fc4415d56994505f4a8
Şamil Nugay <samil_nugay@hotmail.com>
2023-08-28 17:04:01 +0300

    routing

--- server.js
+++ server.js
@@ -1,91 +1,56 @@
-const path = require('path');
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-const {logger} = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
-// custom middleware
+// custom middleware logger
 app.use(logger);
 
-// cross origin resource sharing
-const whilelist = ['https://www.google.com','http://127.0.0.1:5500','http://localhost:3500'];
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
 const corsOptions = {
-  origin: (origin,callback) => {
-    if(whilelist.indexOf(origin) !== -1 || !origin) {
-      callback(null,true) 
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  optionsSuccessStatus: 200
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
 
-
-// build-in middleware to handle urlencoded data
-// in other word, from data:
-// 'content-type: application/x-www.form-urlencoded'
-
-app.use(express.urlencoded({extended: false}))
-
+// built-in middleware for json 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'/public')));
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index.(.html)?',(req,res) => {
-  res.sendFile('./views/index.html',{root:__dirname});
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
-
-app.get('/new-page(.html)?',(req,res) => {
-  res.sendFile(path.join(__dirname,'views','new-page.html'));
-}); 
-
-app.get('/old-page(.html)?',(req,res) => {
-  res.redirect(301,'/new-page.html'); // 302 by default
-}); 
-
-//Route handlers
-
-app.get('/hello(.html)?',(req,res,next) => {
-  console.log('attempted to load hello.html');
-  next();
-},(req,res) => {
-  res.send('Hello World!');
-}); 
-
-const one = (req,res,next) => {
-  console.log('one');
-  next();
-}
-
-const two = (req,res,next) => {
-  console.log('two');
-  next();
-}
-
-const three = (req,res) => {
-  console.log('three');
-  res.send('Finished!')
-}
-
-app.get('/chain(.html)?', [one,two,three])
-
-app.all('*',(req,res) => {
-  res.status(404);
-  if(req.accepts('html')) {
-    res.sendFile(path.join(__dirname,'views','404.html'));
-    
-  }
-  else if(req.accepts('html')) {
-    res.json({error: '404 Not found'})
-  } else {
-    res.type('txt').send('404 Not found')
-  }
-}); 
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
